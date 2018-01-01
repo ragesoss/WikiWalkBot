1d9a7f9d5e979f2d5b74038074c7c14aa44d05bc
Sage Ross <sage@ragesoss.com>
2018-01-01 09:38:04 -0800

    Use replies to form a thread

--- wiki_walk_bot.rb
+++ wiki_walk_bot.rb
@@ -5,11 +5,11 @@ Article.connect_to_database 'wikiwalkbot'
 class WikiWalkBot
   def start_walking
     article = Article.all.sample
-    tweet_and_step(article, 0)
+    tweet_and_step(article, nil, 0)
   end
 
-  def tweet_and_step(article, depth)
-    return if depth > 3
+  def tweet_and_step(article, reply_to_id, depth)
+    return if depth > 6
     article.wikilinks.shuffle.each do |link|
       sentence = article.sentence_with(link)
       next unless sentence.present?
@@ -17,10 +17,10 @@ class WikiWalkBot
       next if sentence.length < 40
       pp "tweeting '#{link}' sentence from '#{article.title}'"
       pp sentence
-      article.tweet(sentence)
+      tweet = article.tweet(sentence, in_reply_to_status_id: reply_to_id)
       next_article = FindArticles.by_title link
       sleep 5
-      return tweet_and_step(next_article, depth + 1)
+      return tweet_and_step(next_article, tweet.id, depth + 1)
     end
   end
 end
